abfeab64953aab022eeb2c7a86fcd5aee1ea7031
RenAverion <averionren82@gmail.com>
2026-02-17 14:53:01 +0800

    Minor font size Adjustment

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <h4 className="font-stm text-black text-2xl md:text-3xl">About Me</h4>
         <hr className="block border-dashed my-2 " />
         <p className="mb-3 font-stm text-sm text-justify md:text-l ">
-          Hi! I’m Ren, a passionate web developer specializing in modern
+            Hi! I’m Ren, a passionate web developer specializing in modern
           front-end technologies and web/mobile application development. I
           graduated from LSPU with a Bachelor’s degree in Information
           Technology, majoring in Web and Mobile Application Development. During
@@ -16,16 +16,16 @@ function About() {
           600-hour internship at Pixel 8 Web Solutions, where I worked on Figma,
           UI/UX design, and project collaboration.
         </p>
-        <p className="mb-3 text-justify font-stm">
-          Professionally, I have contributed to web development and digital
+        <p className="mb-3 font-stm text-sm text-justify md:text-l">
+            Professionally, I have contributed to web development and digital
           solutions at Simple.biz, creating websites using Duda, performing SEO,
           leveraging CRM tools, editing multimedia content, generating logos and
           AI videos, and conducting client research. Additionally, I’ve gained
           experience in AI training at Outlier, further expanding my skill set
           in emerging technologies.
         </p>
-        <p className="mb-3 text-justify font-stm">
-          I enjoy building interactive and visually engaging websites, exploring
+        <p className="mb-3 font-stm text-sm text-justify md:text-l">
+            I enjoy building interactive and visually engaging websites, exploring
           UI/UX trends, and experimenting with creative solutions. My goal is to
           craft web applications that are both functional and delightful for
           users.
